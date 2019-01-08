955baa69cff355fc4f241470c489d3a163573007
idlviv <idlviv@rambler.ru>
2019-01-08 10:45:33 +0200

    +

--- server/controllers/sharedController.js
+++ server/controllers/sharedController.js
@@ -12,11 +12,11 @@ module.exports.sendFeedbackMessage = function(req, res, next) {
     to: 'info@hmade.work',
     subject: 'Питання з сайту',
     text: 'Питання ${feedback.message}. Контакти ${feedback.contacts}. Ім\'я ${feedback.name}',
-    html: '<p><span style="font-weight: bold;">Питання </span>' +
+    html: '<p><span style="font-weight: bold;">Питання: </span>' +
     feedback.message + '</p>' +
-    '<p><span style="font-weight: bold">Контакти </span>' +
+    '<p><span style="font-weight: bold">Контакти: </span>' +
     feedback.contacts + '</p>' +
-    '<p><span style="font-weight: bold">Ім\'я </span>' +
+    '<p><span style="font-weight: bold">Ім\'я: </span>' +
     feedback.name + '</p>',
   };
 
