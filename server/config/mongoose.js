d7107ae6914660930361c65feafb89aa8b7bc529
idlviv <idlviv@rambler.ru>
2019-02-28 19:21:11 +0200

    +

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,24 +7,23 @@ const options = {
   autoIndex: process.env.NODE_ENV !== 'production',
   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
   reconnectInterval: 500, // Reconnect every 500ms
-  // poolSize: 10,
+  poolSize: 50, // Maintain up to 50 socket connections
+  wtimeout: 2500,
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
   useNewUrlParser: true,
-  poolSize: 50, // Maintain up to 50 socket connections
-  wtimeout: 2500,
+
 };
 
 const url = config.get('DB_URL');
 
 mongoose.connect(url, options)
-  .then(
-    () => log.verbose('Connected to db '),
-    (err) => {
-      log.error('Error ' + err);
-      process.exit(1);
-    }
-  );
+    .then(() => log.verbose('Connected to db '),
+        (err) => {
+          log.error('Error ' + err);
+          process.exit(1);
+        }
+    );
 
 
 mongoose.set('useCreateIndex', true);
