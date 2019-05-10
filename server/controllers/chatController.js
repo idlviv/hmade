3a010039b98a03f0d9c323c50bca10108fa1bb6b
idlviv <idlviv@rambler.ru>
2019-05-10 13:58:34 +0300

    +

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,9 +1,11 @@
 const log = require('../config/winston')(module);
+const util = require('util');
 
 module.exports = function(server) {
   io = require('socket.io').listen(server);
   io.sockets.on('connection', function(socket) {
-    log.debug('socket connected');
+
+    log.debug(util.format(socket.handshake));
     // socket.emit('messageFromServer', {message: 'world'});
     socket.on('messageToServer', (data) => {
       data.message = 'server response: ' + data.message;
